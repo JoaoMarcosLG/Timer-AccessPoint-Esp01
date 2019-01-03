7b8b8822def64f10dcec1ae0607019a6fb4e2ebf
Joaomlg <joaomarcos1999jm@gmail.com>
2019-01-03 19:21:24 -0200

    Adição de constantes para os diferentes modos de operação

--- main/main.ino
+++ main/main.ino
@@ -42,7 +42,7 @@
 
 #include "time.h"
 
-// Constantes do projeto
+// Mapeamento da EEPROM
 #define TIMES_COUNT    1
 #define TIMER_MODE     2
 #define TIME_ON_HOUR   3
@@ -71,6 +71,9 @@ Time *times;
 Time time_on;
 
 byte times_count = 0;  // Variavel que armazena qnt de horarios programados
+
+#define TIME_MODE 0
+#define INTERVAL_MODE 1
 
 bool timer_mode = 0;  // Modo: (default) 0 -> horários; 1 -> intervalo
 
@@ -102,7 +105,7 @@ void handleTimeMode();
 // --- MAIN ---
 void setup() {
   // Espera 1s para estabilizacao
-    delay(1000);
+  delay(1000);
 
   // Configura IO's
   pinMode(button, INPUT);
@@ -147,7 +150,7 @@ void loop() {
   
   // Se WiFi ligado...
   if(wifi_status){  // Inicia comunicacao HTTP
-      server.handleClient();
+    server.handleClient();
   }
 }
 
@@ -216,26 +219,43 @@ void EEPROM_read() {
 }
 
 void timeOnCheck() {
-  DateTime now = rtc.now();  // Variavel que contem dados atuais do relogio
+  switch(timer_mode) {
+    case TIME_MODE:
+      DateTime now = rtc.now();  // Variavel que contem dados atuais do relogio
 
-  // Confere se o horario atual "bate" com algum dos horarios de acionamento
-  for(byte i=0; i<times_count; i++) {
-    if(times[i].cmp(now.hour(), now.minute())) { // Se sim...
-      if(last_time != i){
-        last_time = i;  // Atualiza variável do ultimo horario acionado
-        digitalWrite(relay, HIGH);  // Aciona saida
-        relay_status = true;  // Atualiza Flag de estado do relay
+      // Confere se o horario atual "bate" com algum dos horarios de acionamento
+      for(byte i=0; i<times_count; i++) {
+        if(times[i].cmp(now.hour(), now.minute())) { // Se sim...
+          if(last_time != i){
+            last_time = i;  // Atualiza variável do ultimo horario acionado
+            digitalWrite(relay, HIGH);  // Aciona saida
+            relay_status = true;  // Atualiza Flag de estado do relay
+          }
+        }
       }
-    }
+    break;
+
+    case INTERVAL_MODE:
+
+    break;
   }
 }
 
 void timeOffCheck() {
-  Time now = Time(rtc.now()); 
-  Time time_off = times[last_time] + time_on;  // Horario de desacionamento = horario de acionamento + tempo ligado
-  if(now >= time_off) {
-    digitalWrite(relay, LOW);  // Desaciona saida
-    relay_status = false;  // Atualiza Flag de estado do relay
+  switch(timer_mode) {
+    case TIME_MODE:
+      Time now = Time(rtc.now()); 
+      Time time_off = times[last_time] + time_on;  // Horario de desacionamento = horario de acionamento + tempo ligado
+
+      if(now >= time_off) {
+        digitalWrite(relay, LOW);  // Desaciona saida
+        relay_status = false;  // Atualiza Flag de estado do relay
+      }
+    break;
+
+    case INTERVAL_MODE:
+
+    break;
   }
 }
 
@@ -258,18 +278,24 @@ void timeSort() {
 
 Time nextTime() {
   // Se modo 'horarios'
-  if(!timer_mode) {
-    Time now = Time(rtc.now());
+  switch(timer_mode) {
+    case TIME_MODE:
+      Time now = Time(rtc.now());
 
-    // Percorre todos os horarios para verificar proximo horario de acionamento
-    for(byte i=0; i<times_count; i++) {
-      if(now > times[i]) {
-        continue;
-      } else {
-        return times[i];
+      // Percorre todos os horarios para verificar proximo horario de acionamento
+      for(byte i=0; i<times_count; i++) {
+        if(now > times[i]) {
+          continue;
+        } else {
+          return times[i];
+        }
       }
-    }
-    return times[0];  // Se horario atual 'maior' que todos os horarios, entao retorna primeiro horario
+      return times[0];  // Se horario atual 'maior' que todos os horarios, entao retorna primeiro horario
+    break;
+
+    case INTERVAL_MODE:
+
+    break;
   }
 }
 
@@ -322,7 +348,7 @@ void handleTime() {
   String html = "<!doctype html><html lang=\"pt-br\"><head><title>Timer</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"><style>.flex-container {display: flex;flex-direction: column;justify-content: center;}.center-x {display: block;margin-left: auto;margin-right: auto;}.no-margin {margin: 0;}.time-input {display: flex;flex-direction: row;}.time-input input {width: 50vw;margin: 5px !important;}.time-input button {margin-top: 5px;height: 42px;line-height: 30px;}.time-input label { margin: auto; }.risk {height:2px;border:none;color:#212529;background-color:#212529;margin-top: 0px;}html {background: rgb(248, 248, 248);margin-top: 24px;}div, form {margin-bottom: 12px;}input[type=\"text\"] {border-radius: 8px;font-size: 20px;margin-top: 12px;margin-bottom: 18px;height: 36px;padding-left: 10px;}h1, h2, h3, h4, h5, h6 {font-family: -apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, Oxygen, Ubuntu, Cantarell, \"Open Sans\", \"Helvetica Neue\", sans-serif;font-weight: 400;line-height: 1.5;color: #212529;margin: 0;}h1 { font-size: 70px; }h3 { font-size: 40px; }h5 { font-size: 20px; }.btn {display: inline-block;font-weight: 400;text-align: center;white-space: nowrap;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;border: 1px solid transparent;padding: 0.375rem 0.75rem;font-size: 1.6rem;line-height: 1.5;border-radius: 0.25rem;transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;}.btn:focus, .btn.focus {outline: 0;box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);}.btn-primary {color: #fff;background-color: #007bff;border-color: #007bff;}.btn-primary:hover {color: #fff;background-color: #0069d9;border-color: #0062cc;}.btn-success {color: #fff;background-color: #28a745;border-color: #28a745;}.btn-success:hover {color: #fff;background-color: #218838;border-color: #1e7e34;}.btn-danger {color: #fff;background-color: #dc3545;border-color: #dc3545;}.btn-danger:hover {color: #fff;background-color: #c82333;border-color: #bd2130;}.btn-secondary {color: #fff;background-color: #6c757d;border-color: #6c757d;}.btn-secondary:hover {color: #fff;background-color: #5a6268;border-color: #545b62;}</style></head><body><div class=\"flex-container\"><h3 class=\"center-x\">Configurações</h3><div class=\"risk\"></div><h5 class=\"center-x\">Modo: {{ mode }}</h5><h5 class=\"center-x\">{{ info }}</h5><div class=\"center-x\" style=\"margin-top: 12px\"><button class=\"btn btn-secondary\" style=\"width: 90vw\" onclick=\"window.location.replace('mode_select')\">Configurar</button></div><div class=\"center-x\"><button class=\"btn btn-primary\" style=\"width: 90vw\" onclick=\"window.location.replace('/')\">Inicio</button></div></div></body><script>var previous_length = {};function check_time(element, repeat) {id = element.name;if(!(id in previous_length)) { previous_length[id] = 0; }for(let i=0; i < repeat; i++) {if(element.value.length == ((3*i)+2) && element.value.length > previous_length[id]) {element.value += ':';}}previous_length[id] = element.value.length;}var time_count = 1;function add_cell() {let new_cell = document.createElement('div');new_cell.className = 'time-input';new_cell.innerHTML = `<label>Horário ${++time_count}:</label><input name=\"h${time_count}\" type=\"text\" placeholder=\"00:00\" autocomplete=\"off\" maxlength=\"5\" onkeyup=\"check_time(this, 1)\" required><button type=\"button\" class=\"btn btn-success\" style=\"height: 42px\" onclick=\"add_cell()\">+</button>`;document.getElementById('times-container').appendChild(new_cell);new_cell.getElementsByTagName('input')[0].focus();let previous_button = new_cell.previousElementSibling.getElementsByTagName('button')[0];previous_button.className = 'btn btn-danger';previous_button.innerHTML = 'x';previous_button.onclick = function() { remove_cell(previous_button) };}function remove_cell(element) {let times_container = document.getElementById('times-container');times_container.removeChild(element.parentNode);let count = 0;for(let i=0; i < times_container.children.length; i++) {times_container.children[i].firstChild.innerText = `Horário ${++count}:`;}time_count = count;}function sync_clk(form) {let date = new Date();let input_hours = document.createElement('input');input_hours.type = 'text';input_hours.name = 'hours';input_hours.value = date.getHours();input_hours.hidden = true;form.appendChild(input_hours);let input_minutes = document.createElement('input');input_minutes.type = 'text';input_minutes.name = 'minutes';input_minutes.value = date.getMinutes();input_minutes.hidden = true;form.appendChild(input_minutes);let input_seconds = document.createElement('input');input_seconds.type = 'text';input_seconds.name = 'seconds';input_seconds.value = date.getSeconds();input_seconds.hidden = true;form.appendChild(input_seconds);}function verify_request(new_url) {if(location.search != '') {window.location.replace(new_url);}}</script></html>";
 
   // Exibe o modo de funcionamento
-  html.replace("{{ mode }}", (timer_mode ? "Intervalo" : "Horários"));
+  html.replace("{{ mode }}", (timer_mode == INTERVAL_MODE ? "Intervalo" : "Horários"));
 
   // Monta html para exibição de informações cadatradas
   String info = "";
@@ -370,7 +396,7 @@ void handleIntervalMode() {
     }
 
     // Configura variáveis auxiliares
-    timer_mode = 1;  //Atualiza flag do modo de operação
+    timer_mode = INTERVAL_MODE;  //Atualiza flag do modo de operação
 
     times_count = 1;  // Atualiza numero de horarios
     EEPROM_write();  // Salva alteracoes na EEPROM
@@ -383,7 +409,7 @@ void handleIntervalMode() {
     relay_status = false;
   }
 
-  server.send(200, "text/html", "Nothing yet!");
+  server.send(200, "text/html", html);
 }
 
 void handleTimeMode() {
@@ -419,7 +445,7 @@ void handleTimeMode() {
       }
     }
 
-    timer_mode = 0;  //Atualiza flag do modo de operação
+    timer_mode = TIME_MODE;  //Atualiza flag do modo de operação
 
     times_count = j;  // Atualiza numero de horarios
     timeSort();  // Ordena horarios
